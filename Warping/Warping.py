32e3df537612f39caee2d954b61651f57d6ff57d
zer01ike <wangkai_like@163.com>
2018-05-04 20:04:53 +0800

    add depth_map warping result

--- Warping/Warping.py
+++ Warping/Warping.py
@@ -1,38 +1,50 @@
 import numpy as np
+# this is the class for 3D image processing
+# which can make the a image to warp to a new place
+# and contain 1D and 3D ways to do this
+# 3D way need to be done.
+#
+# provided by zer0like<wangkai_like@163.com> at 5.4.2018
+# #
+
+
 class warping():
-    def warpingwith1D(self,TexturedImage,DepthedImage):
+    def warpingwith1D(self, TexturedImage, DepthedImage):
         table = []
-        knear = 0  #the near is 0
-        kfar = 128 #the far is 128
-        eye_seperation = 6 #means the seperation of the two eye is the 6cm
-        view_distance = 800 # the view distance is 3m
-        Npix = 320 #standard definition display to reduce the pararllex
-        S = 25 # depth = 0, maxium shift .
-        height,width,channels = TexturedImage.shape
-        print(width,height,channels)
-        #print(DepthedImage[700,575])
-        warping_image = np.zeros((height,width,3),np.uint8)
-        print (warping_image[500][500])
-        for i in range(0,256):
-            A = i *(knear/64 + kfar/16)/255
-            h = -eye_seperation * Npix * (A - kfar/16) / view_distance
-            table.append(int(h/2))
-        for i in range(0,height):
-            for j in range (0,width):
-                #print(i,j,DepthedImage[i,j])
-                depth_level = int(DepthedImage[i,j])
+        knear = 0  # the near is 0
+        kfar = 128  # the far is 128
+        eye_seperation = 6  # means the seperation of the two eye is the 6cm
+        view_distance = 800  # the view distance is 3m
+        Npix = 320  # standard definition display to reduce the pararllex
+        S = 25  # depth = 0, maxium shift .
+        height, width, channels = TexturedImage.shape
+        warping_image = np.zeros((height, width, 3), np.uint8)
+        warping_depth_image = np.zeros((height, width, 1), np.uint8)
+        for i in range(0, 256):
+            A = i * (knear / 64 + kfar / 16) / 255
+            h = -eye_seperation * Npix * (A - kfar / 16) / view_distance
+            table.append(int(h / 2))
+        for i in range(0, height):
+            for j in range(0, width):
+                # print(i,j,DepthedImage[i,j])
+                depth_level = int(DepthedImage[i, j])
                 shift = table[depth_level]
-                if j + shift -S >= 0 and j+shift-S < width:
-                    warping_image[i][j + shift - S][0] = TexturedImage[i,j][0]
-                    warping_image[i][j + shift - S][1] = TexturedImage[i,j][1]
-                    warping_image[i][j + shift - S][2] = TexturedImage[i,j][2]
-                elif j+shift -S >= 0 and j+shift-S >width:
-                    warping_image[i+1][j + shift - S-i][0] = TexturedImage[i,j][0]
-                    warping_image[i+1][j + shift - S-i][1] = TexturedImage[i,j][1]
-                    warping_image[i+1][j + shift - S-i][2] = TexturedImage[i,j][2]
+                if j + shift - S >= 0 and j + shift - S < width:
+                    for s in range (0,3):
+                        warping_image[i][j + shift - S][s] = TexturedImage[i, j][s]
+                        #make the disclosure area with RGB = (200,200,200)
+                        warping_image[i][j] = 200
+                    warping_depth_image[i][j + shift - S] = DepthedImage[i, j]
+                    # make the  of the area of the
+                    warping_depth_image[i][j]= 255
+                elif j + shift - S >= 0 and j + shift - S > width:
+                    for s in range (0,3):
+                        warping_image[i + 1][j + shift - S - i][s] = TexturedImage[i, j][s]
+                        warping_image[i+1][j-i] = 200
+                    warping_depth_image[i + 1][j + shift - S - i] = DepthedImage[i, j]
+                    warping_depth_image[i + 1][j - i] = 255
 
-        return warping_image
+        return warping_image, warping_depth_image
 
-
-    def warpingwith3D(self,TexturedImage,DepthedImage,CameraParameter):
+    def warpingwith3D(self, TexturedImage, DepthedImage, CameraParameter):
         pass
--- test/testopencv.py
+++ test/testopencv.py
@@ -1,7 +1,9 @@
 import cv2
 import sys
+
 sys.path.append("..")
 from Warping import Warping
+
 # img =  cv2.imread("test.jpg", 0)
 # px = img[2,2]
 # print(px)
@@ -9,11 +11,12 @@ from Warping import Warping
 # cv2.waitKey(0)
 # cv2.destroyAllWindows( )
 
-depthedimg = cv2.imread('depth00300.jpg',0)
-TexturedImg = cv2.imread('image00300.jpg',1)
+depthedimg = cv2.imread('depth00300.jpg', 0)
+TexturedImg = cv2.imread('image00300.jpg', 1)
 
 W1d = Warping.warping()
-output_image = W1d.warpingwith1D(TexturedImg, depthedimg)
-cv2.imshow('image',output_image)
+output_txt_image, output_depth_image = W1d.warpingwith1D(TexturedImg, depthedimg)
+cv2.imshow('image', output_txt_image)
+cv2.imshow('depth', output_depth_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
