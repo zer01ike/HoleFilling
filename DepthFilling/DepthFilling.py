d470288a43c4cb244cdc2cdb9fa60c65f090a7d5
zer01ike <wangkai_like@163.com>
2018-05-07 17:40:19 +0800

    [2018.5.7.17:40]update Depthfilling method more useful

--- DepthFilling/DepthFilling.py
+++ DepthFilling/DepthFilling.py
@@ -1,85 +1,96 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-#Using the ICIP 2010's way to filling the disocusion in the depth map
-#
-#choose i which is not in the distortion area, calculate the M*M windows size area which i is
-#the center of them. then find a j which is in this area and it's in the distortion area.
-#calculate the k-means(k=2) to find the min and max in this area . using this value to overload j's
-#value.
-#
+# Using the ICIP 2010's way to filling the disocusion in the depth map
+# choose i which is not in the distortion area, calculate the M*M windows size area which i is
+# the center of them. then find a j which is in this area and it's in the distortion area.
+# calculate the k-means(k=2) to find the min and max in this area . using this value to overload j's
+# value.
+
+# Attention: the kernel input must be odd number
 
 class DepthFilling() :
-    def DepthFill(self, DepthImage, WindowSize):
-        #step 1: choose i
-        height, width = DepthImage.shape
 
-        h_start = int(0+WindowSize/2)
-        h_end = int(height - WindowSize/2)
+    def depthfill(self, depthimage, windowsize, stepsize):
+        # step 1: choose i
+        height, width = depthimage.shape
 
-        w_start = h_start
-        w_end = int(width - WindowSize/2)
-        height_count = 0
-        width_count = 0
+        vertical_start = int(0 + windowsize/2)
+        vertical_end = int(height - windowsize/2)
 
-        times_count =0
-        totally = (h_end-h_start) * (w_end - w_start)/32/32
-        for i in range(h_start,h_end,int(WindowSize)):
-            for j in range(w_start,w_end,int(WindowSize)):
-                hole_count = 0
-                sample = np.zeros((WindowSize * WindowSize, 2))
-                sample_1d = np.zeros(WindowSize*WindowSize)
+        horizontal_start = vertical_start
+        horizontal_end = int(width - windowsize/2)
+
+        #counting the
+        vertical_count = 0
+        horizontal_count = 0
+
+        # counting the progress
+        times_count = 0
+        totally = (horizontal_end-horizontal_start) * (vertical_end - vertical_start)/stepsize/stepsize
+        
+        for i in range(vertical_start, vertical_end, stepsize):
+            for j in range(horizontal_start, horizontal_end, stepsize):
+                # count the hole numbers
+                number_valid = 0
+                
+                # array to store the kernel content  
+                kernel_content = np.zeros(windowsize * windowsize)
+                
                 # (i,j) is the center of the M*M area
                 # get every value in this area and using K-means to cluster the min and the max
+                half = int(windowsize/2)
+                for kernel_v in range(i-half, i+half+1):
+                    for kernel_h in range(j-half, j+half+1):
+                        #print(vertical_count, horizontal_count)
+                        if depthimage[kernel_v,kernel_h] <= 250:
+                            number_valid += 1
+                        index = vertical_count * windowsize + horizontal_count
+                        #print(index)
+                        kernel_content[index] = depthimage[kernel_v, kernel_h]
+                        horizontal_count += 1
+                    horizontal_count = 0
+                    vertical_count += 1
+                vertical_count = 0
+                
+                kernel_without_hole = np.zeros(number_valid)
 
-                for temp_i in range(int(i-WindowSize/2), int(i+WindowSize/2)):
-                    for temp_j in range(int(j-WindowSize/2), int(j+WindowSize/2)):
-                        #print(height_count, width_count)
-                        if DepthImage[temp_i,temp_j] == 255:
-                            hole_count +=1
-                        sample[height_count * WindowSize + width_count][0] = DepthImage[temp_i, temp_j]
-                        sample[height_count * WindowSize + width_count][1] = DepthImage[temp_i, temp_j]
-                        sample_1d[height_count * WindowSize + width_count] = DepthImage[temp_i, temp_j]
-                        width_count += 1
-                    width_count = 0
-                    height_count += 1
-                height_count = 0
-
-                sample_without_hole = np.zeros((sample.size-hole_count,2))
-                sample_without_hole_1d = np.zeros(sample.size-hole_count)
                 count_without_hole = 0
-                for x in range(0,int(sample.size/2)):
-                    if sample[x][0] != 255 :
-                        sample_without_hole[count_without_hole][0] = sample[x][0]
-                        sample_without_hole[count_without_hole][1] = sample[x][1]
-                        sample_without_hole_1d[count_without_hole]= sample[x][0]
-                        count_without_hole+=1
+                NoZero = False
+                for x in range(0, int(kernel_content.size)):
+                    if kernel_content[x] <= 250 :
+                        kernel_without_hole[count_without_hole] = kernel_content[x]
+                        count_without_hole += 1
 
-                est =  KMeans(2)
-                est.fit(sample_without_hole_1d.reshape(-1,1))
-                center = est.cluster_centers_
-                print (center)
-                if center[1][0] > center[0][0] :
-                    c_min = center[0][0]
-                else:
-                    c_min = center[1][0]
-                for temp_i in range(int(i-WindowSize/2), int(i+WindowSize/2)):
-                    for temp_j in range(int(j-WindowSize/2), int(j+WindowSize/2)):
-                        if DepthImage[temp_i,temp_j] == 255:
-                            DepthImage[temp_i, temp_j] = int(DepthImage[i, j]) if DepthImage[i, j] <= c_min else int(c_min)
+                        if kernel_content[x] !=0 :
+                            NoZero = True
+
+                if NoZero == True:
+                    #print(kernel_without_hole)
+                    est =  KMeans(2)
+                    est.fit(kernel_without_hole.reshape(-1,1))
+                    center = est.cluster_centers_
+
+                    c_min = center[1] if center[1] < center[0] else center[0]
+                    #print(c_min)
+                    for kernel_v in range(i-half, i+half+1):
+                        for kernel_h in range(j-half, j+half+1):
+                            if depthimage[kernel_v,kernel_h] >=250:
+                                depthimage[kernel_v, kernel_h] = int(depthimage[i, j]) if depthimage[i, j] <= c_min else int(c_min)
                 times_count += 1
+
             print(str(round(times_count/totally*100,3))+"%"+"Processed")
-        return DepthImage
+        return depthimage
 
 
-    def testKmeans(self,DepthImage):
-        height, width = DepthImage.shape
-        #print(height,width,DepthImage[int(height/2), int(width/2)])
+    def testKmeans(self,depthimage):
+        height, width = depthimage.shape
+        #print(height,width,depthimage[int(height/2), int(width/2)])
         sample=np.zeros(width*height)
         for i in range(0, height):
             for j in range(0, width):
                 #calcute the hole image's min and max
-                sample[i*height+j] = DepthImage[i,j]
+                sample[i*height+j] = depthimage[i,j]
         print(sample)
         est = KMeans(2)
         est.fit(sample.reshape(-1,1))
@@ -92,10 +103,10 @@ class DepthFilling() :
 
         for i in range(0, height):
             for j in range(0, width):
-                if DepthImage[i, j] >= 250 :
-                    DepthImage[i, j] = int(DepthImage[int(height/2), int(width/2)]) if DepthImage[int(height/2), int(width/2)]<=c_min else int(c_min)
+                if depthimage[i, j] >= 250 :
+                    depthimage[i, j] = int(depthimage[int(height/2), int(width/2)]) if depthimage[int(height/2), int(width/2)]<=c_min else int(c_min)
 
-        return DepthImage
+        return depthimage
 
 
 
--- test/testDepthfilling.py
+++ test/testDepthfilling.py
@@ -4,9 +4,9 @@ import cv2
 DepthedImg = cv2.imread('distation_depth00300.jpg', 0)
 
 DF = DepthFilling.DepthFilling()
-depth_filled =  DF.testKmeans(DepthedImg)
-#depth_filled  = DF.DepthFill(DepthedImg,32)
+#depth_filled =  DF.testKmeans(DepthedImg)
+depth_filled  = DF.depthfill(DepthedImg,5,5)
 cv2.imshow('depth', depth_filled)
-cv2.imwrite('depth32_new.jpg',depth_filled)
+cv2.imwrite('depth5_new.jpg',depth_filled)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
